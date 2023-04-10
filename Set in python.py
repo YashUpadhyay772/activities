80260127317fcc7c76367592a6ce404ace646fa8
YashUpadhyay772 <127281863+YashUpadhyay772@users.noreply.github.com>
2023-04-10 17:06:08 +0000

    main

--- Set in python.py
+++ Set in python.py
@@ -1,14 +1,31 @@
-sentence = "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drik"
-print(sentence, "\n")
-
 # split sentence into list of words
-sentence_list = "You will have to fill out the function" 
+sentence_list =  # You will have to fill out the function 
 print(sentence_list, '\n')
 
 # convert list to set to get unique words
-sentence_set = "But why this function needs to be stuffed"
+sentence_set = 
 print(sentence_set, '\n')
 
 # print the number of unique words
-num_unique = "PJY210121"
+num_unique = 
 print(num_unique, '\n')
+'''
+
+sentence = "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drik"
+
+# split sentence into list of words
+
+sentence_list = []
+words = sentence.split()
+sentence_list.extend(words)
+print(sentence_list,"\n")
+
+# convert list to set to get unique words
+
+sentence_set = set(sentence_list)
+print(sentence_set,"\n")
+
+# print the number of unique words
+
+num_unique = len(sentence_set)
+print(num_unique)
