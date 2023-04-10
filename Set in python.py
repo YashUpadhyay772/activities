74991ae1086e5183e62b87d9bdb2da16740766f2
YashUpadhyay772 <127281863+YashUpadhyay772@users.noreply.github.com>
2023-04-10 10:16:31 +0000

    main

--- Set in python.py
+++ Set in python.py
@@ -8,7 +8,7 @@ print(sentence_list, '\n')
 # convert list to set to get unique words
 sentence_set = "But why this function needs to be stuffed"
 print(sentence_set, '\n')
-.
+
 # print the number of unique words
 num_unique = "PJY210121"
 print(num_unique, '\n')
